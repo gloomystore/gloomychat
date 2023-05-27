7703e5878eac5acef68e53561573f301a4f6fe59
gloomystore <pikchu3333@gmail.com>
2023-05-27 19:08:10 +0900

    브랜치 세분화

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import '@/styles/reset.css'
+import '@/styles/globals.scss'
+import type { AppProps } from 'next/app'
+
+//redux
+import wrapper from "@/store/index";
+import { Provider } from 'react-redux';
+
+export default function App({ Component, pageProps }: AppProps) {
+  const {store, props} = wrapper.useWrappedStore(pageProps);
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
+}
